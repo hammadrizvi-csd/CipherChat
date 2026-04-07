b7161ea6b22470bdb3594d00fa78283d4a9cdc96
Hammad <Hammadrizvi346@gmail.com>
2026-04-07 18:54:53 +0530

    Make a Server.js file

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// Imports Routes & Socket Handler
+const apiRoutes = require("./routes/apiRoutes");
+const socketHandler = require("./socket/socketHandler");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 🔥 PRO TRICK: Controllers ko Socket.io ka access dene ke liye
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+// Saare APIs is router se handle honge
+app.use("/", apiRoutes);
+
+// Default Route
+app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "public", "index.html")); });
+
+// Socket.io ka Logic
+socketHandler(io);
+
+// Database Connection
+mongoose.connect("mongodb://asheerrizvi346_db_user:chatapp123@ac-yigke4u-shard-00-00.mqp9wb9.mongodb.net:27017,ac-yigke4u-shard-00-01.mqp9wb9.mongodb.net:27017,ac-yigke4u-shard-00-02.mqp9wb9.mongodb.net:27017/chatapp?ssl=true&replicaSet=atlas-iktg3j-shard-0&authSource=admin&retryWrites=true&w=majority")
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.log("DB error:", err));
+
+const PORT = 4000;
+server.listen(PORT, "0.0.0.0", () => { console.log(`Server running at http://localhost:${PORT}`); });
